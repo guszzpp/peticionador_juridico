6ebb7b2f5211a06d7638628079f7ad8889a5579d
Gustavo L. <gustavolippi@gmail.com>
2025-05-14 16:24:31 -0300

    chore(gitignore): removida 'Nova pasta' e arquivos_gerados e arquivos_upload do controle de versão

--- Nova pasta/src/peticionador/modelos/interfaces/servico_resumidor.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class ServicoResumidor(ABC):
-    @abstractmethod
-    def resumir(self, texto: str) -> str:
-        ...
--- Nova pasta/src/peticionador/utilitarios/configuracoes.py
+++
@@ -1,3 +0,0 @@
-from decouple import config
-
-GEMINI_API_KEY: str = config("GEMINI_API_KEY", default="__MISSING__")
